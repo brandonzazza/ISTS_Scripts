bac07a780213a412db5fbbd611b6c85ed606fa5e
ahm8475 <55161488+21alecmiller@users.noreply.github.com>
2022-03-03 22:31:52 -0500

    Update win_usr.ps1

--- win_usr.ps1
+++ win_usr.ps1
@@ -1,4 +1,4 @@
-#usage .\win_usr.ps1 user
+#usage .\win_usr.ps1 <scoring user>
 
 param (
 	[Parameter()]
@@ -7,3 +7,22 @@ param (
 
 #disable and remove from admin group all users except $keep
 #add to admin group and change password for $keep
+
+$users = (Get-WmiObject -Class Win32_UserAccount | Select-Object -ExpandProperty name)
+foreach ($u in $users) {
+	if ($u -eq $keep) {
+		net user $u /active:yes > $null 2>&1
+		net localgroup administrators $u /add > $null 2>&1
+		echo "change $u's password:"
+		net user $u $(Read-Host -AsSecureString) > $null 2>&1
+		echo "scoring user $u set up"
+	} else {
+		net user $u /active:no > $null 2>&1
+		net localgroup administrators $u /delete > $null 2>&1
+		echo "$u disabled"
+	}
+}
+
+#turn on all default firewalls
+netsh AdvFirewall set AllProfiles state on > $null 2>&1
+echo "firewalls enabled"
