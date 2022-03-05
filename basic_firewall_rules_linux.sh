e8849e5e16b2822196c16bff1b5cbaf07d956a33
Nerelod <dapperfellowdaniel@gmail.com>
2022-03-05 07:44:48 -0500

    updated with team number

--- basic_firewall_rules_linux.sh
+++ basic_firewall_rules_linux.sh
@@ -4,16 +4,16 @@
 
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 ##white list
-iptables -A INPUT -m iprange --src-range 10.x.1.1-10.x.1.255 -j ACCEPT
-iptables -A INPUT -m iprange --src-range 172.16.x.1-172.16.x.21 -j ACCEPT
+iptables -A INPUT -m iprange --src-range 10.2.1.1-10.2.1.255 -j ACCEPT
+iptables -A INPUT -m iprange --src-range 172.16.2.1-172.16.2.21 -j ACCEPT
 
 ##
 iptables -A INPUT -p tcp --dport 53 -j ACCEPT
 iptables -A INPUT -p tcp -j DROP
 
 #output rules
-iptables -A OUTPUT -m iprange --src-range 10.x.1.1-10.x.1.255 -j ACCEPT
-iptables -A OUTPUT -m iprange --src-range 172.16.x.1-172.16.x.21 -j ACCEPT
+iptables -A OUTPUT -m iprange --src-range 10.2.1.1-10.2.1.255 -j ACCEPT
+iptables -A OUTPUT -m iprange --src-range 172.16.2.1-172.16.2.21 -j ACCEPT
 iptables -A OUTPUT -p tcp -j DROP
 #default policy
 
